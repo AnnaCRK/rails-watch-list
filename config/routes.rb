254b987c472ccf0cf1b568b65fa50c0a589d2e53
Anna Karsten <anna-karsten@hotmail.com>
2024-02-22 17:04:08 +0100

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,25 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+  
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
+
+
+#   root "lists#index"
+#   get "lists/new", to: "lists#new"
+#   get "bookmarks/new", to: "bookmarks#new"
+#   get "lists/:id", to: "lists#show"
+
+#   post "lists", to: "lists#create"
+#   post "bookmarks", to: "bookmarks#create"
+#   # resources :lists, only: [:index]
+#   # resources :bookmarks, only: [:]
+
+# end
+
+# resources :restaurants do
+#   resources :reviews, only: [:new, :create]
